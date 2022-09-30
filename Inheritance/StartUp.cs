01a8acea0ea8425c079c315e4fa02e259a8fbd20
Bogomil Simitchiev <111244883+Bogomil-Simitchiev@users.noreply.github.com>
2022-10-01 00:00:45 +0300

    Added examples in the main method

--- Inheritance/StartUp.cs
+++ Inheritance/StartUp.cs
@@ -7,7 +7,31 @@ namespace Farm
     {
         static void Main(string[] args)
         {
-           
+            //Examples
+             StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            stackOfStrings.AddRange(new string[] { "Bogi","Bogi"});
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            
+             RandomList randomList = new RandomList();
+
+            randomList.Add("1");
+            randomList.Add("2");
+            randomList.Add("3");
+
+            Console.WriteLine(randomList.RandomString());
+            randomList.RemoveRandomString();
+            Console.WriteLine(randomList.Count);
+            
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+       
 
         }
     }
