f13aa6df00ee1f63927a14baf6706a3e79fcdfa1
Bogomil Simitchiev <simitchiev365@abv.bg>
2022-10-08 18:07:04 +0300

    Added exercises for Interfaces

---
+++ InterfacesAndAbstraction/Cars/StartUp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+
+namespace Cars // Note: actual namespace depends on the project name.
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start();
+        public string Stop();
+
+    }
+    public interface IElectricCar
+    {
+        public int Battery { get; set; }
+    }
+
+
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get ; set; }
+        public string Color { get; set ; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString();
+        }
+
+    }
+    class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString();
+        }
+    }
+
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+
+
+
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/Shapes/StartUp.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shapes
+{  
+
+
+    public interface IDrawable
+    {
+        public void Draw();
+
+    }
+    class Circle : IDrawable
+    {
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        private int radius;
+
+        public int Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+
+        public void Draw()
+        {
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+            for (int i = this.radius; i >= -this.radius; --i)
+            {
+                for (double j = -this.radius; j < rOut; j+=0.5)
+                {
+                    double value = j * j + i * i;
+                    if (value>=rIn*rIn&&value<=rOut*rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public class Rectangle : IDrawable
+    {
+        public Rectangle(int width,int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        private int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height-1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+                
+            }
+            DrawLine(this.width, '*', '*');
+        }
+        private void DrawLine(int width,char end,char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width-1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(r);
+
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            IDrawable rectangle = new Rectangle(w,h);
+
+            rectangle.Draw();
+
+            circle.Draw();
+        }
+    }
+}
