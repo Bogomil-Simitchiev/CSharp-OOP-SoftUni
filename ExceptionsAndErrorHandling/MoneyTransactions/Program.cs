47aedfd8b3ea018c21cff0cc12aa941e070d85b6
Bogomil Simitchiev <simitchiev365@abv.bg>
2022-10-17 18:18:54 +0300

    Added 2 new exercises practising 'try-catch'

---
+++ ExceptionsAndErrorHandling/MoneyTransactions/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyApp
+{
+    public class BankAccount
+    {
+        public int AccountNumber { get; set; }
+
+        public double AccountBalance { get; set; }
+       
+
+        public BankAccount(int number, double balance)
+        {          
+            this.AccountNumber = number;
+            this.AccountBalance = balance;
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1-45.67,2-3256.09,3-97.34
+
+
+            List<BankAccount> accounts = new List<BankAccount>();
+
+            string[] input = Console.ReadLine().Split(",");
+
+            foreach (string item in input)
+            {
+                string[] split = item.Split("-");
+
+                BankAccount acc = new BankAccount(int.Parse(split[0]),double.Parse(split[1]));
+                accounts.Add(acc);
+            }
+
+
+            while (true)
+            {
+                
+                string[] command = Console.ReadLine().Split(" ");
+
+                string action = command[0];
+
+                if (action=="End")
+                {
+                    break;
+                }
+
+                try
+                {
+                    if (action != "Withdraw" && action != "Deposit")
+                    {
+                        throw new Exception("Invalid command!");
+                    }
+                    else if (action == "Withdraw" || action == "Deposit")
+                    {
+                        switch (action)
+                        {
+                            case "Withdraw":
+
+                                int currentNum = int.Parse(command[1]);
+                                double sum = double.Parse(command[2]);
+
+                                bool isThere2 = false;
+
+                                foreach (BankAccount account in accounts)
+                                {
+                                    if (account.AccountNumber == currentNum)
+                                    {
+                                        isThere2 = true;
+                                        if (account.AccountBalance > sum)
+                                        {
+
+                                            account.AccountBalance -= sum;
+                                            Console.WriteLine($"Account {account.AccountNumber} has new balance: {account.AccountBalance:f2}");
+                                            Console.WriteLine("Enter another command");
+                                        }
+                                        else
+                                        {
+                                            throw new Exception("Insufficient balance!");
+                                        }
+                                    }
+                                }
+                                if (isThere2 == false)
+                                {
+                                    throw new Exception("Invalid account!");
+                                }
+
+
+                                break;
+                            case "Deposit":
+                                int currentNumAcc = int.Parse(command[1]);
+                                double sum2 = double.Parse(command[2]);
+
+                                bool isThere = false;
+
+                                foreach (BankAccount account in accounts)
+                                {
+                                    if (account.AccountNumber == currentNumAcc)
+                                    {
+                                        isThere = true;
+                                        account.AccountBalance += sum2;
+                                        Console.WriteLine($"Account {account.AccountNumber} has new balance: {account.AccountBalance:f2}");
+                                        Console.WriteLine("Enter another command");
+                                    }
+                                }
+                                if (isThere == false)
+                                {
+                                    throw new Exception("Invalid account!");
+                                }
+                                break;
+                        }
+
+
+                    }
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Enter another command");
+                }
+                
+                
+                
+            }
+
+
+        }
+    }
+}
---
+++ ExceptionsAndErrorHandling/PlayCatch/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int count = 0;
+
+
+            while (count != 3)
+            {
+                string[] split = Console.ReadLine().Split();
+
+                try
+                {
+                    if (split.Length == 3)
+                    {
+
+                        if (split[0] == "Replace")
+                        {
+                            int index = int.Parse(split[1]);
+                            int element = int.Parse(split[2]);
+                            list[index] = element;
+                            
+
+                        }
+                        else if (split[0] == "Print")
+                        {
+                            int start = int.Parse(split[1]);
+                            int end = int.Parse(split[2]);
+                            Console.WriteLine(String.Join(", ",list.GetRange(start,end-start+1)));
+                        }
+
+                    }
+                    else if (split.Length == 2)
+                    {
+                        int index = int.Parse(split[1]);
+                        Console.WriteLine(list[index]);
+                    }
+                }
+                catch (FormatException)
+                {
+                    count++;
+                    Console.WriteLine("The variable is not in the correct format!");
+                }
+
+                catch (Exception)
+                {
+                    count++;
+                    Console.WriteLine("The index does not exist!");
+                }
+
+
+
+
+            }
+
+
+            Console.WriteLine(String.Join(", ", list));
+
+
+
+
+        }
+    }
+}
