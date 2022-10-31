ff452ecb912e72baf9ab86fbc5334b40d3f3da41
Bogomil Simitchiev <simitchiev365@abv.bg>
2022-10-31 21:57:30 +0200

    Added more unit tests for the current exercise

--- UnitTesting/Tests/UnitTest1.cs
+++ UnitTesting/Tests/UnitTest1.cs
@@ -6,12 +6,54 @@ namespace Tests
     public class Tests
     {
         [Test]
-        public void TestDurability()
+        public void CheckChangingDurability()
         {
             Axe axe = new Axe(10, 10);
             Dummy dummy = new Dummy(10, 10);
             axe.Attack(dummy);
             Assert.That(axe.DurabilityPoints,Is.EqualTo(9),"Axe Durability doesn't change after attack.");
         }
+        [Test]
+        public void TestSum()
+        {
+            StartUp st = new StartUp();
+
+            int result=st.Sum(1, 2);
+
+            Assert.AreEqual(3,result);
+
+        }
+
+        [Test]
+        public void TestMultiply()
+        {
+            StartUp st = new StartUp();
+
+            int result = st.Multiply(3, 2);
+
+            Assert.AreEqual(6, result);
+
+        }
+
+        [Test]
+      
+        public void DummyLosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(15, 10);
+
+            dummy.TakeAttack(6);
+
+            Assert.That(dummy.Health, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.That(dummy.GiveExperience(), Is.EqualTo(10));
+            
+        }
+
     }
 }
